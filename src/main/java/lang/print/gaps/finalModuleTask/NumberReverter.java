9f4c9e6608028597971dfb020ceb3417fa6ef95e
kenes <kenesvip@gmail.com>
2023-09-04 12:39:54 +0500

    Module #3 Final Task Done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int num = 0;
+        while (number > 0){
+            int endNum = number % 10;
+            num = num * 10 + endNum;
+            number /= 10;
+        }
+        System.out.println(num);
     }
 }
